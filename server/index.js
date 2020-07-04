e9927483b0f28cc7cdd94bab15d5027edff138ec
Supreet Sethi <supreet.s.sethi@gmail.com>
2020-07-05 02:15:43 +0530

    added close functionality to server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
 module.exports = function () {
+  let appServer;
   let server = express(),
     create,
-    start;
+    start,
+    close;
 
   create = function (config) {
     let routes = require("./routes");
@@ -17,16 +18,6 @@ module.exports = function () {
     server.set("mongodb", config.mongodb);
     server.set("viewDir", config.viewDir);
 
-    //connect to the database
-    let mongoDbConfig = server.get("mongodb");
-    let mongoDbConnectionString = `${mongoDbConfig.url}/${mongoDbConfig.databaseName}?retryWrites=true&w=majority`;
-    mongoose
-      .connect(mongoDbConnectionString, { useNewUrlParser: true })
-      .then(() => console.log(`Database connected successfully`))
-      .catch((err) => console.log(err));
-
-    //since mongoose promise is depreciated, we overide it with node's promise
-    mongoose.Promise = global.Promise;
     // Returns middleware that parses json
     // server.use(bodyParser.json());
 
@@ -53,15 +44,39 @@ module.exports = function () {
     let hostname = server.get("hostname"),
       port = server.get("port");
 
-    server.listen(port, function () {
-      console.log(
-        "Express server listening on - http://" + hostname + ":" + port
-      );
+    //connect to the database
+    let mongoDbConfig = server.get("mongodb");
+    let mongoDbConnectionString = `${mongoDbConfig.url}/${mongoDbConfig.databaseName}?retryWrites=true&w=majority`;
+    mongoose
+      .connect(mongoDbConnectionString, { useNewUrlParser: true })
+      .then(() => {
+        if (server.get("env") != "test")
+          console.log(`Database connected successfully`);
+      })
+      .catch((err) => console.log(err));
+
+    //since mongoose promise is depreciated, we overide it with node's promise
+    mongoose.Promise = global.Promise;
+    appServer = server.listen(port, function () {
+      if (server.get("env") != "test")
+        console.log(
+          "Express server listening on - http://" + hostname + ":" + port
+        );
+    });
+  };
+  close = function (callback) {
+    appServer.close(function () {
+      mongoose.disconnect().then(() => {
+        if (server.get("env") != "test")
+          console.log(`Database connected successfully`);
+      });
+      if (callback) callback();
     });
   };
 
   return {
     create: create,
     start: start,
+    close: close,
   };
 };
