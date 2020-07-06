5c27599195d3d750aa00db2ab0d44664b8a0f2c8
Supreet Sethi <supreet.s.sethi@gmail.com>
2020-07-06 20:46:15 +0530

    using logger package that was created

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import {Tracker} from "../../logger/"
 
+var mylogger = Tracker();
+mylogger.init();
+//mylogger.init('my key goes here')
 ReactDOM.render(<App />, document.getElementById("root"));
