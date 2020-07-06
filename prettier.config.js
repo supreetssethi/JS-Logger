4f346c5be1635f02f15c4a927594773bf1c65070
Supreet Sethi <supreet.s.sethi@gmail.com>
2020-07-07 01:20:13 +0530

    added husky and lint staged to format and fix lint issues of all the files befote commitinh

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,7 @@
-/* eslint-disable prettier/prettier */
 module.exports = {
   tabWidth: 2,
   semi: true,
   singleQuote: false,
-  trailingComma: "es5",
+  trailingComma: "all",
+  endOfLine: "auto",
 };
